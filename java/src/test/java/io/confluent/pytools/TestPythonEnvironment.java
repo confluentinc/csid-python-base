2819b43acf9a5c120685d3807e6b3adfda630ec4
Laurent Domenech-Cabaud <laurent@confluent.io>
2023-06-15 17:30:21 +0200

    cleanup + more tests

--- java/src/test/java/io/confluent/pytools/TestPythonEnvironment.java
+++ java/src/test/java/io/confluent/pytools/TestPythonEnvironment.java
@@ -10,7 +10,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class TestPythonEnvironment {
     public static void deleteDirectory(File directory) {
@@ -47,6 +46,41 @@ class TestPythonEnvironment {
         PythonEnvironment pyEnv = PythonEnvironment.build(
                 new String[]{"algorithms==0.1.4", "find-libpython==0.3.0", "pemja==0.3.0"}, Paths.get(tmpDir),
                 Paths.get(defaultPythonPath), null, null);
+
+        String envPython = pyEnv.getPythonExePath();
+        System.out.println("Virtual environment created with python = " + envPython);
+
+        Assertions.assertTrue(Files.exists(Paths.get(envPython)));
+
+        String sitePackages = PythonEnvironment.getSitePackages(envPython);
+        System.out.println("and site packages = " + sitePackages);
+
+        Path pemjaLib = Paths.get(sitePackages, "pemja");
+        Assertions.assertTrue(Files.exists(pemjaLib));
+
+        System.out.print("Calling python code using pemja...");
+        // test the installed env --> run some python code
+        pyEnv.executePythonStatement("import algorithms.strings as s");
+        Object res = pyEnv.callPythonMethod("s.decode_string", "3[a]2[bc]");
+        assertEquals(res, "aaabcbc");
+        System.out.println("OK");
+
+        // remove temp venv folder
+        deleteDirectory(new File(tmpDir));
+    }
+
+    @SneakyThrows
+    @Test
+    void venvInstallNamedEnv() {
+        // use volatile java temp dir instead of /tmp/
+        String tmpDir = Files.createTempDirectory(null).toFile().getAbsolutePath();
+        System.out.println("Temp Directory: " + tmpDir);
+
+        String defaultPythonPath = PythonEnvironment.defaultPythonExecutablePath().toString();
+
+        PythonEnvironment pyEnv = PythonEnvironment.build(
+                new String[]{"algorithms==0.1.4", "find-libpython==0.3.0", "pemja==0.3.0"}, Paths.get(tmpDir),
+                Paths.get(defaultPythonPath), "venv1", null);
 
         String envPython = pyEnv.getPythonExePath();
         System.out.println("Virtual environment created with python = " + envPython);
