17da6db7f0cb1b3d97fca2c0cffd5b95b7508b9a
Laurent Domenech-Cabaud <laurent@confluent.io>
2023-06-20 19:31:41 +0200

    shorter adv test

--- java/src/test/java/io/confluent/pytools/TestPythonHost.java
+++ java/src/test/java/io/confluent/pytools/TestPythonHost.java
@@ -47,15 +47,7 @@ public class TestPythonHost {
     @SneakyThrows
     @Test
     void advancedEntryPoint() {
-        // test files are in resources (requirements.txt + test.py)
-        Path requirementsFile = Paths.get("src","test","resources", "requirements.txt");
-        Assertions.assertTrue(Files.exists(requirementsFile));
-
-        Path pythonScript = Paths.get("src","test","resources", "adv1", "adv2.py");
-        Assertions.assertTrue(Files.exists(pythonScript));
-
         String pythonExecutable = PyUtils.defaultPythonExecutablePath().toString();
-
         Path scriptsDirectory = Paths.get("src","test","resources");
 
         PythonHost host = new PythonHost(pythonExecutable, scriptsDirectory.toFile(), "adv1.adv2.hello", ".");
